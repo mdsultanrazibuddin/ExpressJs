55b310e662940267bad41279c4a91c37fc895931
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-07-13 16:10:59 +0600

    added

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+
+
+
+
+app.listen(4545, () =>{
+    console.log('hello listener');
+})
--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require ('express')
-
-const app = express()
-
-
-
-app.listen(5555, () =>{
-    console.log('server is running');
-})
