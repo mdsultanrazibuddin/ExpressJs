43d5a5ac9ea6dadb2ea68bdc89b10111cd2be7dd
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-07-13 17:06:06 +0600

    added

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-const express = require('express')
+const { error } = require('console');
+const express = require ('express');
+const fs = require('fs')
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) =>{
-    res.send(' i am a programmer')
+
+app.get('/', (req, res) => {
+    fs.readFile('./pages/index.html', (err, data) =>{
+        if(err) {
+            console.log('Error', err);
+            res.send(' <h2>something is wrong</h2> ')
+        }
+        else{
+            res.write(data)
+            res.end
+        }
+    })
+})
+app.get('/home', (req, res) => {
+    res.send('i am programer')
+})
+app.get('/about', (req, res) => {
+    res.send('i am pro')
 })
 
 
 
 
 
-app.listen(4545, () =>{
-    console.log('hello listener');
+
+
+app. listen(5252, ( ) => {
+    console.log('hi listener');
 })
