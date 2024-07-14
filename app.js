83717d594faa61636ec100bf89c810832483fd1d
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-07-14 15:36:16 +0600

    'added'

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 
 const express = require ('express');
+const morgan = require ('morgan');
+const cors = require ('cors');
 const fs = require('fs')
 
 const app = express();
-
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors())
 
 app.get('/', (req, res) => {
     
